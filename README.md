bd10be01b2c079e7c83baa17d53da51d2caf18d5
Joannic Laborde <joannic@sparkart.com>
2014-09-23 15:04:55 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 gulp-filerev-replace
 ====================
+
+Rev and replace files according to their mutual dependencies.
+
+Usage:
+
+```javascript
+gulp
+  .src(['assets/**/*', '!assets/{scripts,styles}/**/*', 'views/**/*'], {base: process.cwd()})
+  .pipe(filerevReplace({
+    filerev: ['assets/**/*'],
+    replace: ['assets/compiled/**/*', 'views/**/*'],
+    base:    'assets'}))
+  .pipe(gulp.dest(dest));
+
+// Output
+Starting 'gulp-filerev-replace'...
+gulp-filerev-replace Filerevved: assets/images/ajax-loader.gif -> assets/images/ajax-loader-aef3c727.gif
+gulp-filerev-replace Replaced: assets/compiled/scripts.js (9)
+gulp-filerev-replace Replaced: assets/compiled/styles.css (8)
+gulp-filerev-replace Replaced: views/index.html (6)
+gulp-filerev-replace Replaced: views/media/index.html (5)
+gulp-filerev-replace Filerevved: assets/compiled/styles.css -> assets/compiled/styles-b4051241.css
+gulp-filerev-replace Replaced: assets/compiled/scripts.js (1)
+gulp-filerev-replace Replaced: views/index.html (1)
+gulp-filerev-replace Filerevved: assets/compiled/scripts.js -> assets/compiled/scripts-0328d21d.js
+gulp-filerev-replace Replaced: views/index.html (1)
+Finished 'gulp-filerev-replace' after 88 ms
+```
+
+Options:
+
+- `filerev`: [minimatch](https://github.com/isaacs/minimatch) pattern to filter the files to filerev.
+- `replace`: [minimatch](https://github.com/isaacs/minimatch) pattern to filter the files to replace.
+- `base`: Directory from where the files are served by the web server.
+
+TODO:
+
+- Add tests!
---
+++ index.js
@@ -0,0 +1,185 @@
+var path           = require('path');
+var through        = require('through2');
+var gutil          = require('gulp-util');
+var lazypipe       = require('lazypipe');
+var crypto         = require('crypto');
+var multimatch     = require('multimatch');
+var istextorbinary = require('istextorbinary');
+
+const PLUGIN_NAME        = 'gulp-filerev-replace';
+const STARTING_DELIMITER = '(\\\\?\'|\\\\?"|\\\\?\\()';
+const ENDING_DELIMITER   = '(\\\\?\'|\\\\?"|\\\\?\\)|\\?|#)';
+
+function plugin(options) {
+  var self = {};
+
+  return (lazypipe()
+    .pipe(initialize.bind(self), options)
+    .pipe(findReferences.bind(self))
+    .pipe(sortByReferencesDepth.bind(self))
+    .pipe(filerevAndReplace.bind(self))
+  )();
+};
+
+function initialize(options) {
+  var self = this;
+
+  self.filerev = {};
+  self.replace = {};
+  self.options = options || {};
+  self.options.filerev || (self.options.filerev = '**/*');
+  self.options.replace || (self.options.replace = '**/*');
+
+  return transformAllFiles(function(file, enc) {
+    if (multimatch(file.relative, self.options.filerev).length) {
+      self.filerev[file.relative] = {
+        regexp: new RegExp(STARTING_DELIMITER + escapeRegExp(webPath.call(self, file)) + ENDING_DELIMITER, 'g'),
+        references: []
+      };
+    }
+
+    if (multimatch(file.relative, self.options.replace).length && istextorbinary.isTextSync(null, file.contents)) {
+      self.replace[file.relative] = {
+        contents: String(file.contents)
+      };
+    }
+  });
+};
+
+function findReferences() {
+  var self = this;
+
+  return transformAllFiles(function(file, enc) {
+    if (self.replace[file.relative]) {
+      for (var src in self.filerev) {
+        if (self.filerev[src].regexp.test(self.replace[file.relative].contents)) {
+          self.filerev[src].references.push(file.relative);
+        }
+      }
+    }
+  });
+};
+
+function sortByReferencesDepth() {
+  var self   = this;
+  var depths = {};
+
+  return transformAllFiles(
+    function(file, enc) {
+      // Find the max references depth for each file
+      var filerev = self.filerev[file.relative];
+      try {
+        depths[file.relative] = filerev ? maxDepth.call(self, [file.relative], filerev.references) : 0;
+      } catch(err) {
+        this.emit('error', new gutil.PluginError(PLUGIN_NAME, err));
+      }
+    },
+    function(files) {
+      // Sort by decreasing depth
+      return files.sort(function(file1, file2) {
+        return depths[file2.relative] - depths[file1.relative];
+      });
+    }
+  );
+};
+
+function filerevAndReplace() {
+  var self = this;
+
+  return through.obj(function(file, enc, cb) {
+    var old_relative = file.relative;
+
+    if (self.filerev[old_relative]) {
+      file.path = filerevFile(file);
+
+      gutil.log(PLUGIN_NAME, 'Filerevved: ' + old_relative + ' -> ' + file.relative);
+
+      var references = self.filerev[old_relative].references;
+      var regexp  = self.filerev[old_relative].regexp;
+      var dest    = webPath.call(self, file);
+
+      for (var i = 0; i < references.length; ++i) {
+        var reference = references[i];
+        var count  = 0;
+
+        self.replace[reference].contents = self.replace[reference].contents.replace(regexp, function(match, starting_delimiter, ending_delimiter) {
+          ++count;
+          return starting_delimiter + dest + ending_delimiter;
+        });
+
+        gutil.log(PLUGIN_NAME, 'Replaced: ' + reference + ' (' + count + ')');
+      }
+    }
+
+    if (self.replace[old_relative]) {
+      file.contents = new Buffer(self.replace[old_relative].contents);
+    }
+
+    this.push(file);
+    cb();
+  });
+};
+
+// Transform all files in the stream but wait until all files are handled before emitting events
+function transformAllFiles(transform, flush) {
+  var files = [];
+
+  return through.obj(
+    function(file, enc, cb) {
+      if (file.isStream()) {
+        this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+        return cb();
+      }
+
+      if (file.isBuffer()) {
+        if (transform) transform.call(this, file, enc);
+      }
+
+      files.push(file);
+      cb();
+    },
+    function(cb) {
+      if (flush) files = flush.call(this, files);
+      for (var i = 0; i < files.length; ++i) {
+        this.push(files[i]);
+      }
+      cb();
+    }
+  );
+}
+
+function escapeRegExp(str) {
+  return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
+function webPath(file) {
+  var self = this;
+  return '/' + path.relative(self.options.base || file.base, file.relative);
+};
+
+function maxDepth(stack, references) {
+  if (!references.length) return stack.length;
+
+  var self = this;
+  var depths = references.map(function(src) {
+    var filerev = self.filerev[src];
+    if (!filerev) return stack.length;
+    if (stack.indexOf(src) > -1) throw('Circular reference detected:\n' + stack.concat([src]).join('\n'));
+    return maxDepth.call(self, stack.concat([src]), filerev.references);
+  });
+
+  return Math.max.apply(Math, depths);
+};
+
+function filerevFile(file) {
+  var hash     = md5(file.contents).slice(0, 8);
+  var ext      = path.extname(file.path);
+  var filename = path.basename(file.path, ext) + '-' + hash + ext;
+  return path.join(path.dirname(file.path), filename);
+};
+
+function md5(str) {
+  return crypto.createHash('md5').update(str).digest('hex');
+};
+
+module.exports = plugin;
